6c1b808f1554e90ce338702ba8c0a74e430cab96
hcastillopnt <horacio.castillopnt@gmail.com>
2018-05-09 14:23:30 -0500

    Script de base de datos
    Se agrego un archivo que contiene el script para la creacion de la base
    de datos en mysql.

---
+++ Viper.NET_Script.sql
@@ -0,0 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-12-2017 a las 00:16:03
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";												
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: viper_developments`
+--
+
+CREATE DATABASE `viper_developments`;
+
+USE `viper_developments`;
+
+-- --------------------------------------------------------
