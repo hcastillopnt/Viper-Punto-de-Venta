81383ababc8fa6777a30b662078d2ae384483f12
juan jose castro torrez <castro971@live.com>
2018-06-26 10:02:00 -0500

    Se agrego el API REST para loguearse en la aplicacion Viper Movil.

--- Viper/Viper.WebAPI/Models/ViperContext.cs
+++ Viper/Viper.WebAPI/Models/ViperContext.cs
@@ -17,10 +17,18 @@ namespace Viper.WebAPI.Models
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Productinventory> productinventories { get; set; }
+        public DbSet<EmployeeDepartmentHistory> EmployeeDepartmentHistories { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+           // modelBuilder.Entity<Membership>().HasRequired(x => x.Employee).WithMany().HasForeignKey(x => x.EmployeeId);
+
         }
     }
 }
