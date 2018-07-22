31ea565be5c6271a7b95f7d8d54ae8add3c01776
hcastillopnt <horacio.castillopnt@gmail.com>
2018-07-21 19:47:18 -0500

    Se añadio una clase llamada UtilMessages para guardar todos los mensajes para interactuar con el usuario, desde la interfaz grafica de usuario

---
+++ Viper/Viper.DesktopApp/UtilMessages.cs
@@ -0,0 +1,22 @@
+﻿#region using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Viper.DesktopApp
+{
+    public class UtilMessages
+    {
+        //Mensajes de confirmacion en la interfaz grafica
+        public static string RESULT_COMPANY_SUCESS = "EL NEGOCIO HA QUEDADO REGISTRADO CORRECTAMENTE EN NUESTRA APLICACION VIPER";
+
+        //Mensajes de error en la interfaz grafica
+
+        //Mensajes de informacion en los controles de la interfaz grafica
+    }
+}
