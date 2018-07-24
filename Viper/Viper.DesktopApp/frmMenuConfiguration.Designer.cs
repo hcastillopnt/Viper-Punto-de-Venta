48e28d39dedaedeae82d8f2f75e173f89bab9e34
hcastillopnt <horacio.castillopnt@gmail.com>
2018-07-24 00:08:07 -0500

    Se agrego la interfaz para mostrar el menu de opciones de configuracion

---
+++ Viper/Viper.DesktopApp/frmMenuConfiguration.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Viper.DesktopApp
+{
+    partial class frmMenuConfiguration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuConfiguration));
+            this.visualStudio2012LightTheme1 = new Telerik.WinControls.Themes.VisualStudio2012LightTheme();
+            this.tlpPrincipal = new System.Windows.Forms.TableLayoutPanel();
+            this.picLogotipoApp = new System.Windows.Forms.PictureBox();
+            this.tlpCenter = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpCentro = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogotipoApp)).BeginInit();
+            this.tlpCenter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpPrincipal
+            // 
+            this.tlpPrincipal.BackColor = System.Drawing.SystemColors.Window;
+            this.tlpPrincipal.ColumnCount = 1;
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpPrincipal.Controls.Add(this.tlpCenter, 0, 1);
+            this.tlpPrincipal.Controls.Add(this.picLogotipoApp, 0, 0);
+            this.tlpPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.tlpPrincipal.Name = "tlpPrincipal";
+            this.tlpPrincipal.RowCount = 2;
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.23288F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.76712F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpPrincipal.Size = new System.Drawing.Size(1350, 730);
+            this.tlpPrincipal.TabIndex = 0;
+            // 
+            // picLogotipoApp
+            // 
+            this.picLogotipoApp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picLogotipoApp.Image = ((System.Drawing.Image)(resources.GetObject("picLogotipoApp.Image")));
+            this.picLogotipoApp.Location = new System.Drawing.Point(3, 3);
+            this.picLogotipoApp.Name = "picLogotipoApp";
+            this.picLogotipoApp.Size = new System.Drawing.Size(1344, 149);
+            this.picLogotipoApp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogotipoApp.TabIndex = 2;
+            this.picLogotipoApp.TabStop = false;
+            // 
+            // tlpCenter
+            // 
+            this.tlpCenter.BackColor = System.Drawing.SystemColors.Window;
+            this.tlpCenter.ColumnCount = 3;
+            this.tlpCenter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tlpCenter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tlpCenter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tlpCenter.Controls.Add(this.tlpCentro, 1, 0);
+            this.tlpCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpCenter.Location = new System.Drawing.Point(3, 158);
+            this.tlpCenter.Name = "tlpCenter";
+            this.tlpCenter.RowCount = 1;
+            this.tlpCenter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpCenter.Size = new System.Drawing.Size(1344, 569);
+            this.tlpCenter.TabIndex = 5;
+            // 
+            // tlpCentro
+            // 
+            this.tlpCentro.ColumnCount = 5;
+            this.tlpCentro.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCentro.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCentro.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCentro.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCentro.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCentro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpCentro.Location = new System.Drawing.Point(204, 3);
+            this.tlpCentro.Name = "tlpCentro";
+            this.tlpCentro.RowCount = 4;
+            this.tlpCentro.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCentro.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCentro.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCentro.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCentro.Size = new System.Drawing.Size(934, 563);
+            this.tlpCentro.TabIndex = 0;
+            // 
+            // frmMenuConfiguration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 730);
+            this.ControlBox = false;
+            this.Controls.Add(this.tlpPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMenuConfiguration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMenuConfiguration";
+            this.TopMost = true;
+            this.tlpPrincipal.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogotipoApp)).EndInit();
+            this.tlpCenter.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Telerik.WinControls.Themes.VisualStudio2012LightTheme visualStudio2012LightTheme1;
+        private System.Windows.Forms.TableLayoutPanel tlpPrincipal;
+        private System.Windows.Forms.PictureBox picLogotipoApp;
+        private System.Windows.Forms.TableLayoutPanel tlpCenter;
+        private System.Windows.Forms.TableLayoutPanel tlpCentro;
+    }
+}
