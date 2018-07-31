8c528b9e44f152803611f3881ee88bfde5e83afc
Horacio Enrique Castillo Puente <horacio.castillopnt@bis.ute.edu.mx>
2018-07-31 13:26:56 -0500

    Se modificaron los atributos de la clase user ya que tenia errores

--- Viper/Viper.BusinessEntities/User.cs
+++ Viper/Viper.BusinessEntities/User.cs
@@ -73,25 +73,25 @@ namespace Viper.BusinessEntities
         /// Campo IsWelcome
         /// </summary>
         [Required(ErrorMessage = "El campo IsWelcome es obligatorio")]
-        public byte IsWelcome { get; set; }
+        public bool IsWelcome { get; set; }
 
         /// <summary>
         /// Campo IsApproved
         /// </summary>
         [Required(ErrorMessage = "El campo IsApproved es obligatorio")]
-        public byte IsApproved { get; set; }
+        public bool IsApproved { get; set; }
 
         /// <summary>
         /// Campo IsActive
         /// </summary>
         [Required(ErrorMessage = "El campo IsActive es obligatorio")]
-        public byte IsActive { get; set; }
+        public bool IsActive { get; set; }
 
         /// <summary>
         /// Campo IsEnabled
         /// </summary>
         [Required(ErrorMessage = "El campo IsEnabled es obligatorio")]
-        public byte IsEnabled { get; set; }
+        public bool IsEnabled { get; set; }
 
         /// <summary>
         /// Campo EmployeeId
