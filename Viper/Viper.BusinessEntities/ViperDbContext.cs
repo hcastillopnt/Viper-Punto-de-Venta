b307d91bbe920e34e299d53a42e39d34efff06fb
Horacio Enrique Castillo Puente <horacio.castillopnt@bis.ute.edu.mx>
2018-08-11 15:23:43 -0500

    Se cambio el nombre de la clase Context, ademas de añadir una estrategia de inicializacion

---
+++ Viper/Viper.BusinessEntities/ViperDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viper.BusinessEntities
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class ViperDbContext : DbContext
+    {
+        public ViperDbContext() : base("ViperContext")
+        {
+            Database.SetInitializer<ViperDbContext>(
+                new DropCreateDatabaseIfModelChanges<ViperDbContext>());
+
+            //Database.SetInitializer<ViperDbContext>(
+            //    new CreateDatabaseIfNotExists<ViperDbContext>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
