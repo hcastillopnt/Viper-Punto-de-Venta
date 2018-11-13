5b3b5cbdcd4701d9912a1e2ec6cad39566d104e6
Horacio Enrique Castillo Puente <horacio.castillopnt@bis.ute.edu.mx>
2018-11-12 18:50:39 -0600

    Se elimino la clase de frmLoadingStart y se configuro la ventana de inicio

--- Viper/Viper.DesktopApp/Program.cs
+++ Viper/Viper.DesktopApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
@@ -17,9 +18,52 @@ namespace Viper.DesktopApp
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLoadingStart());
+            ConnectionStringSettingsCollection connections = ConfigurationManager.ConnectionStrings;
+            String connectionName = String.Empty;
+
+            if (connections.Count != 0)
+            {
+                foreach (ConnectionStringSettings connection in connections)
+                {
+                    connectionName = connection.Name;
+                }
+            }
+
+            string connectionString = ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                MessageBox.Show(new Form { TopMost = true }, "La cadena de conexion no ha sido configurada", "Sistema de Punto de Venta Viper-OwalTek Innovation Solutions", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(connectionString);
+                    connection.Open();
+                    Console.WriteLine("MySQL version: " + connection.ServerVersion);
+                    connection.Close();
+
+                    BusinessLogicLayer.MenuBLL.procUploadPermissionsToMenuByAdministrator();
+                    BusinessLogicLayer.MenuBLL.procUploadPermissionsToMenuByBasic();
+
+                    bool isExistsCompany = false;
+
+                    isExistsCompany = BusinessLogicLayer.CompanyBLL.procIsCompanyRegistered();
+
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+
+                    if (isExistsCompany)
+                        Application.Run(new frmLogin());
+                    else
+                        Application.Run(new frmRegisterCompany(true));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(new Form { TopMost = true }, ex.Message, "Sistema de Punto de Venta Viper-OwalTek Innovation Solutions", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
