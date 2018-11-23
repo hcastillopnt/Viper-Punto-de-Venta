5e51c0cfeae5f29ae7d000d5194b1c966f7b55c2
Horacio Enrique Castillo Puente <horacio.castillopnt@bis.ute.edu.mx>
2018-11-23 11:35:41 -0600

    Se ajustaron los script de la base de datos

--- ViperDevDb_Testing_Values.sql
+++ ViperDevDb_Testing_Values.sql
@@ -1,39 +1,20 @@
-/*
--- Query: SELECT * FROM viper_developments.address
-LIMIT 0, 50000
+--
+-- Volcado de datos para la tablas de prueba
+--
 
--- Date: 2018-10-28 18:29
-*/
-INSERT INTO `address` (`Id`,`RoadTypeId`,`AddressTypeId`,`AddressLine1`,`AddressLine2`,`CountryRegionId`,`StateProvinceId`,`CityId`,`PostalCode`,`CreatedDate`,`CreatedBy`,`LastUpdatedDate`,`LastUpdatedBy`) VALUES (1,5,2,'CALLE: DIAMANTE, NO. EXT: 285;','COLONIA: PRIVADAS EL JARAL;',1,19,957,'66834','2018-10-28 00:00:00','HECP','2018-10-28 00:00:00','HECP');
+INSERT INTO `address` (`Id`, `RoadTypeId`, `AddressTypeId`, `AddressLine1`, `AddressLine2`, `CountryRegionId`, `StateProvinceId`, `CityId`, `PostalCode`, `CreatedDate`, `CreatedBy`, `LastUpdatedDate`, `LastUpdatedBy`) VALUES
+(1, 5, 2, 'CALLE: DIAMANTE, NO. EXT: 285;', 'COLONIA: PRIVADAS EL JARAL;', 1, 19, 957, '66834', '2018-10-28 00:00:00', 'HECP', '2018-10-28 00:00:00', 'HECP'),
+(2, 3, 9, 'CALLE: S. BERNABÉ, NO. EXT: 205;', 'COLONIA: VALLE MORELOS;', 1, 19, 985, '64180', '2018-11-23 10:54:17', 'HECP', '2018-11-23 10:54:17', 'HECP');
 
-/*
--- Query: SELECT * FROM viper_developments.addresssat
-LIMIT 0, 50000
-
--- Date: 2018-10-28 18:29
-*/
 INSERT INTO `addresssat` (`Id`,`RoadTypeId`,`AddressTypeId`,`AddressLine1`,`AddressLine2`,`CountryRegionId`,`StateProvinceId`,`CityId`,`PostalCode`,`CreatedDate`,`CreatedBy`,`LastUpdatedDate`,`LastUpdatedBy`) VALUES (1,5,2,'CALLE: MIMAS, NO. EXT: 111;','COLONIA: COSMOPOLIS;',1,19,953,'66612','2018-10-28 00:00:00','HECP','2018-10-28 00:00:00','HECP');
 
-/*
--- Query: SELECT * FROM viper_developments.user
-LIMIT 0, 50000
+INSERT INTO `user` (`Id`, `LoginID`, `PasswordEncrypted`, `PasswordQuestion`, `PasswordAnswer`, `RoleId`, `AccessFailedCount`, `IsWelcome`, `IsActive`, `CreatedDate`, `CreatedBy`, `LastUpdatedDate`, `LastUpdatedBy`) VALUES
+(1, 'CAPH930421PM4', 'Mgq71kVNtNqdFwsiQhe1dw==', NULL, NULL, 1, 0, 1, 1, '2018-10-28 17:22:29', 'HECP', '2018-10-28 17:22:29', 'HECP'),
+(2, 'PFR050609CC92', 'Mgq71kVNtNqdFwsiQhe1dw==', NULL, NULL, 3, 0, 1, 1, '2018-11-23 11:12:02', 'HECP', '2018-11-23 11:12:02', 'HECP');
 
--- Date: 2018-10-28 18:28
-*/
-INSERT INTO `user` (`Id`,`LoginID`,`PasswordEncrypted`,`PasswordQuestion`,`PasswordAnswer`,`RoleId`,`AccessFailedCount`,`IsWelcome`,`IsActive`,`CreatedDate`,`CreatedBy`,`LastUpdatedDate`,`LastUpdatedBy`) VALUES (1,'CAPH930421PM4','Mgq71kVNtNqdFwsiQhe1dw==',NULL,NULL,1,0,1,1,'2018-10-28 17:22:29','HECP','2018-10-28 17:22:29','HECP');
-
-/*
--- Query: SELECT * FROM viper_developments.company
-LIMIT 0, 50000
-
--- Date: 2018-10-28 18:29
-*/
 INSERT INTO `company` (`Id`,`CompanyName`,`CompanyKey`,`AddressId`,`AddressSATId`,`RegimenFiscalId`,`PhoneNumber`,`CellphoneNumber`,`EmailAddress`,`FiscalName`,`RFC`,`CURP`,`ApiKey`,`BusinessActivity`,`UserId`,`Logo`,`CreatedDate`,`CreatedBy`,`LastUpdatedDate`,`LastUpdatedBy`) VALUES (1,'SUPER FARMACIAS FARMANYL','EMPOWTK0001',1,1,1,'8121650892','8120127009','PATRICIA.PEREZARN@OUTLOOK.COM','HORACIO ENRIQUE CASTILLO PUENTE','CAPH930421PM4','CAPH930421HNLSNR01',NULL,'FARMACIA',1,NULL,'2018-10-28 00:00:00','HECP','2018-10-28 00:00:00','HECP');
 
-/*
--- Query: SELECT * FROM viper_developments.site
-LIMIT 0, 50000
-
--- Date: 2018-10-28 18:29
-*/
 INSERT INTO `site` (`Id`,`CompanyId`,`SiteName`,`UniquePhysicalID`,`AddressId`,`ResponsibleName`,`PhoneNumber`,`IsActive`,`CreatedDate`,`CreatedBy`,`LastUpdatedDate`,`LastUpdatedBy`) VALUES (1,1,'DEFAULT','OWTK-SUPFARFARPM4NL',1,'DEFAULT','8121650892',1,'2018-10-28 00:00:00','HECP','2018-10-28 00:00:00','HECP');
+
+INSERT INTO `supplier` (`Id`, `SupplierName`, `SupplierKey`, `AddressId`, `PhoneNumber`, `CellphoneNumber`, `EmailAddress`, `RFC`, `BusinessActivity`, `UserId`, `IsActive`, `Logo`, `CreatedDate`, `CreatedBy`, `LastUpdatedDate`, `LastUpdatedBy`) VALUES
+(1, 'RASE FARMACÉUTICA', 'OWTK-RASFARC92NL', 2, '8183111103', '0180063848982', 'INFO@RASE.COM.MX', 'PFR050609CC92', 'DISTRIBUIDORA DE MEDICAMENTOS', 2, 0, NULL, '2018-11-23 10:54:17', 'HECP', '2018-11-23 10:54:17', 'HECP');
